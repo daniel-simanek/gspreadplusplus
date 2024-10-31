15298924605e343bf339fff72d5d72a1c7a873ae
Daniel Simanek <daniel.simanek@decathlon.com>
2024-10-31 19:46:26 +0000

    Added py modules

--- setup.py
+++ setup.py
@@ -2,13 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='gspreadplusplus',
-    version='1.0.0',
+    version='1.0.2',
     author='Daniel Simanek',
     author_email='daniel.simanek@decathlon.com',
     description='Advanced functions using the gspread library, used for making the code less bloated.',
     long_description=open('README.md').read(),  # Long description from the README file
     long_description_content_type='text/markdown',
+    url='https://github.com/daniel-simanek/gspreadplusplus',
     packages=find_packages(),
+    py_modules=['gspreadplusplus'],
     install_requires=[
         'gspread',
         'oauth2client',
