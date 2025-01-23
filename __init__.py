166ecba2b9d71dc04514a09c7cb3274509065f97
Dan Simanek <dandadin@gmail.com>
2025-01-23 16:30:40 +0100

    Version 2.1.3
    added support for short, smallint and tinyint

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
 from .gspreadplusplus import GPP
 
-__version__ = '2.1.2'
+__version__ = '2.1.3'
 __all__ = ['GPP']
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gspreadplusplus',
-    version='2.1.2',
+    version='2.1.3',
     author='Daniel Simanek',
     author_email='daniel.simanek@decathlon.com',
     description='Enhanced Google Sheets operations with advanced data type handling',
